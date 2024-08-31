324bad4179f2b54a417763d0e16f21f77ae00b96
DEll <DEll@DESKTOP-EERHPE6>
2024-08-31 22:48:32 +0300

    Add project files.

---
+++ project_one/Models/Users.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace project_one.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        //=======================================
+        [Required]
+        public string? Name { get; set; }
+        //======================================
+        [EmailAddress]
+        public string? Email { get; set; }
+        //==================================
+        public int? Password { get; set; }
+        //=====================================
+        public string? Privlliage { get; set; }
+    }
+}
